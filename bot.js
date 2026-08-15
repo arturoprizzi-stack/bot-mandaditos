9c8bcc7396a1da19524ca8092640a169b68008f5
arturoprizzi-stack <arturo.prizzi@gmail.com>
2026-08-15 08:58:23 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const { default: makeWASocket, useMultiFileAuthState, fetchLatestBaileysVersion, DisconnectReason } = require("@whiskeysockets/baileys")
 const P = require("pino")
-
+const qrcode = require("qrcode-terminal")
 async function startBot() {
     const { version } = await fetchLatestBaileysVersion()
     const { state, saveCreds } = await useMultiFileAuthState("auth_info")
@@ -8,14 +8,14 @@ async function startBot() {
         version, 
         auth: state, 
         logger: P({ level: "silent" }), 
-        printQRInTerminal: true, 
         browser: ["Chrome", "Windows", "10"] 
     })
 
     sock.ev.on("creds.update", saveCreds)
 
     sock.ev.on("connection.update", (update) => {
-        const { connection, lastDisconnect } = update
+        const { connection, lastDisconnect, qr } = update
+        if(qr) qrcode.generate(qr, {small: true})
         if(connection === 'close') {
             const shouldReconnect = lastDisconnect?.error?.output?.statusCode !== DisconnectReason.loggedOut
             console.log('Conexion cerrada, reconectando:', shouldReconnect)
