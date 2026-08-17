d546d7f05a023a39a92d4e99e321d0c1e551d5dc
arturoprizzi-stack <arturo.prizzi@gmail.com>
2026-08-17 12:23:29 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -98,7 +98,7 @@ const KEYWORDS = {
   carretita: ["tacos la carretita"],
   aldente: ["quete","quette","muralla","saljo","saljoo","sajo","sajoo","olla","que te late","que te latte"],
   tacosalex: ["tacos alex"],
-  quesera: ["quesera","qesera","queseria","qseria","quecera","qecera","qsera","qcera","quesria"]
+  quesera: ["quesera","qesera","queseria","qseria","quecera","qecera","qsera","qcera","quesria","qeseria","qesria","qcseria"]
 };
 
 // Cargar estado guardado de ON/OFF (si existe) al arrancar
