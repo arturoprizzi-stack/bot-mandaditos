3d26cc3ff71316eadd17d946a9b7530dabba59a9
arturoprizzi-stack <arturo.prizzi@gmail.com>
2026-08-17 02:33:12 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -53,32 +53,32 @@ const RESTAURANTES = {
   villafit: {
     nombre: "VILLAFIT", grupo: "Veloces 2", activo: true,
     contactosNombre: ["VILLAFIT", "VILLAFIT2"],
-    numeros: ["526699128588", "526691220281"]
+    numeros: ["526699128588", "526691220281", "163015651504220", "110951940513841", "139372007534721"] // último es identificador de PRUEBA, quitar después
   },
   saboria: {
     nombre: "MENUDO DOÑA LUPE SABORIA", grupo: "Veloces 2", activo: true,
     contactosNombre: ["MENUDO*SANCHEZ", "MENUDO*SANCHEZ2"],
-    numeros: ["526691484113", "526691222437"]
+    numeros: ["526691484113", "526691222437", "127152607494351", "196095523168257"]
   },
   roll: {
     nombre: "LA CASA DEL ROLL", grupo: "Veloces 5", activo: true,
     contactosNombre: ["ROLES*SANCHEZC", "ROLES*SANCHEZ"],
-    numeros: ["526691491778"]
+    numeros: ["526691491778", "241055324704912"]
   },
   carretita: {
     nombre: "TACOS LA CARRETITA", grupo: "Veloces 2", activo: true,
     contactosNombre: ["TACOS*ESTADIO"],
-    numeros: ["526691172841", "139372007534721"] // último es identificador de PRUEBA, quitar después
+    numeros: ["526691172841", "177158962057264"]
   },
   maz: {
     nombre: "MAZ SALADS", grupo: "MAZ SALADS TOREO", activo: true,
     contactosNombre: ["BRENDASALADS", "MAZ SALADS", "MAZSALADS", "MAZ SALADS TOREO", "MAZSALADS TOREO"],
-    numeros: ["526692514582"]
+    numeros: ["526692514582", "124451660255334"]
   },
   aldente: {
     nombre: "ALDENTE", grupo: "Al Dente Pedidos", activo: true,
     contactosNombre: ["ALDENTE", "ALDENTE3", "IRVING"],
-    numeros: ["526692699876", "526691619067", "526692705147"]
+    numeros: ["526692699876", "526691619067", "526692705147", "26766370467858", "1464768458800", "169535344791699"]
   }
 };
 
